92736ad92554ea7b70e3331addf546edff515f85
Elena Sierra <esierra@stanford.edu>
2024-08-13 11:21:09 -0700

    one line swap

--- temp_elena_src/supervised_train.py
+++ temp_elena_src/supervised_train.py
@@ -285,8 +285,6 @@ def train(args, save_dir, full_exp_id, exp_id):
         model = models.resnet50(pretrained=True)
     else:
         model = models.resnet18(pretrained=True)
-        
-    params = model.parameters()
     
     if args.train_type == 'feature_extraction':
         for param in model.parameters():
@@ -294,6 +292,8 @@ def train(args, save_dir, full_exp_id, exp_id):
         model.fc = nn.Linear(model.fc.in_features, len(label_dict))
         for param in model.fc.parameters():
             param.requires_grad = True
+        
+    params = model.parameters()
     
     model.to(device)
 
