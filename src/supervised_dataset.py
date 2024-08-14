7a3a9e922d0651fcd4467732a180588d6c408d48
Lauren Gillespie <leg2015@gmail.com>
2024-08-14 05:43:32 -0700

    moving to src folder and reverting folder name for laurens models

---
+++ src/supervised_dataset.py
@@ -0,0 +1,59 @@
+"""
+File: supervised_dataset.py
+Authors: Elena Sierra & Lauren Gillespie
+------------------
+Defines the PyTorch dataset classes
+"""
+
+# torch packages
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import torchvision
+import torchvision.transforms as transforms
+
+# misc packages
+import os 
+import json 
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from PIL import Image
+
+# ----------------- Dataset classes ----------------- #
+
+# class providing tensor of image and its label for the DivShift dataset
+class LabelsDataset(Dataset):
+    def __init__(self, data_frame, img_dir, label_dict, to_classify, transform, target_transform=None):
+        self.img_labels = data_frame
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+        self.label_dict = label_dict
+        self.to_classify = to_classify
+        
+    def __len__(self):
+        return len(self.img_labels)
+    
+    def __getitem__(self, idx):
+        # determine image path
+        photo_id = str(self.img_labels.iloc[idx]['photo_id'])
+        folder = photo_id[:3]
+        state = self.img_labels.iloc[idx]['state_name']
+        img_path = self.img_dir + "/" + state + "/" + folder + "/" + photo_id + ".png"
+
+        # open and transform image
+        img_og = Image.open(img_path)
+        tensor_img = transforms.ToTensor()(img_og)
+        tensor_img = tensor_img[:3, : , : ]
+        if tensor_img.shape[0] < 3:  # Check the number of channels
+            tensor_img = tensor_img.repeat(3, 1, 1)
+        if self.transform:
+            tensor_img = self.transform(tensor_img)
+
+        # collect label
+        label = self.label_dict[self.img_labels.iloc[idx][self.to_classify]]
+        if self.target_transform:
+            label = self.target_transform(label)
+        
+        return tensor_img, label
