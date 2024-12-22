bbde3b3d99b070619ad62f14856cdc34d667d9ef
Lauren Gillespie <leg2015@gmail.com>
2024-12-21 16:24:18 -0800

    cleaning up old files

--- src/params.py
+++
@@ -1,8 +0,0 @@
-
-
-
-class params():
-    def __init__(args):
-        self.lr = args.lr
-        self.num_batches = args.num_batches
-        self.num_epochs = args.num_epochs
--- src/utils.py
+++
@@ -1,5 +0,0 @@
-
-
-
-def accuracy(guess, actual):
-    return None
